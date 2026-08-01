a5d72dd13ec7f65647685e17c100164111a836b1
Dmitri Wamback <56200546+DmitriWamback@users.noreply.github.com>
2026-08-01 11:17:38 -0400

    Added all US state models

--- static/src/shader_sources.h
+++ static/src/shader_sources.h
@@ -1,7 +1,12 @@
 const char* debugVertexShaderSource = R"(#version 300 es
 layout(location = 0) in vec2 position;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = projection * view * model * vec4(position, 0.0, 1.0);
 }
 )";
 
